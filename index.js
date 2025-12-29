24a4d5bfeb4fb7d94ec1df40252e6df40c441f90
link-short <dora.family228@gmail.com>
2025-12-29 19:19:23 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const urlpargmgsg = new URLSearchParams(window.location.search);
+const asset_id = urlpargmgsg.get('id');
+
+if (asset_id) {
+  console.log(type + " port");
+  loadAndInsertContent('https://winnigames2024-original.github.io/assets/' + asset_id, 'content-container');
+}
+
+async function loadAndInsertContent(url, targetDivId) {
+  try {
+    const response = await fetch(url);
+    const htmlContent = await response.text(); // Получаем HTML как текст
+
+    const targetDiv = document.getElementById(targetDivId);
+
+    targetDiv.insertAdjacentHTML('beforeend', htmlContent);
+
+    } catch (error) {
+    console.error('Ошибка при загрузке или вставке контента:', error);
+    }
+}

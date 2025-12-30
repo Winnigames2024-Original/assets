bb10353b106cf594eaa568e58413dca201b2b106
link-short <dora.family228@gmail.com>
2025-12-30 14:44:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function loadAndInsertContent(url, targetDivId) {
     const htmlContent = await response.text(); // Получаем код как текст
 
     const targetDiv = document.getElementById(targetDivId);
-    allCodeRest = JSON.stringify(response.text());
+    allCodeRest = htmlContent;
     targetDiv.insertAdjacentHTML('beforeend', htmlContent);
 
     } catch (error) {
